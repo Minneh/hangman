ab040b54ec3203badc3e7aebc522fc52e35d34dd
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-21 12:36:43 +0300

    add comment to HangManTest file

--- src/test/java/HangManTest.java
+++ src/test/java/HangManTest.java
@@ -1,15 +1,15 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HangManTest{
+
+  // check if HangMan instantiates correctly 
   @Test
   public void newHangMan_instantiatesCorrectly(){
     HangMan testHangMan = new HangMan("boy", "lsd");
     assertEquals(true, testHangMan instanceof HangMan);
   }
 
-  // public void newRectangle_instantiatesCorrectly() {
-  //   Rectangle testRectangle = new Rectangle(2, 4);
-  //   assertEquals(true, testRectangle instanceof Rectangle);
-  // }z
 }
