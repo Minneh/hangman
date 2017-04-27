d85ce1b94552f3e077a798ea1d621810fe1d63d0
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-27 08:53:09 +0300

    add instance of Hangman class to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,12 +2,11 @@ import java.io.Console;
 
 public class App{
 
-
   public static void main(String[] args) {
     Console myConsole = System.console();
     System.out.println("Welcome to Hangman!");
-    System.out.println("Please enter a letter to guess the word!:\n");
-    String stringUserNumber = myConsole.readLine();
-    // Hangman newHangman = new Hangman(userInput);
+    System.out.println("Please enter a letter to guess the word!:");
+    String userInput = myConsole.readLine();
+    Hangman newHangman = new Hangman(userInput);
   }
 }
