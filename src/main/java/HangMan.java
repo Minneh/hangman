cb9c3dd94fd683ee53bcfb723fa747f6dc9aaa47
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-11 11:50:51 +0300

    refactor Hangman'

--- src/main/java/HangMan.java
+++ src/main/java/HangMan.java
@@ -1,9 +1,75 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Hangman{
 
   //declare private variables to hold the answer word and letters the uers has entered respectively
 
   private String mAnswer;
   private String mPastLetters;
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+    String[] guesses = {"reddit", "programming", "fruit", "doughnut"};
+
+    boolean weArePlaying = true;
+
+    while(weArePlaying){
+      System.out.println("Welcome to my Hangman game!");//reddit ->r, e, d, d, i, t
+      char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();
+      int amountOfGuesses = randomWordToGuess.length;
+      char[] playerGuess = new char[amountOfGuesses];//creates empty char array where we will save user input
+      for (int i = 0; i< playerGuess.length; i++) {
+        playerGuess[i] = '_';
+      }
+      boolean wordIsGuessed = false;
+      int tries = 0;
+
+      while(!wordIsGuessed && tries != amountOfGuesses){
+        System.out.print("Current guesses: ");
+        printArray(playerGuess);
+        System.out.printf("You have %d tries left.\n", amountOfGuesses - tries);
+        System.out.println("Enter a single character:");
+        char input = scanner.nextLine().charAt(0);
+        tries++;
+        if(input == '-'){
+          weArePlaying = false;
+          wordIsGuessed = true;
+        } else{
+          for (int i = 0; i< randomWordToGuess.length; i++) {
+            if(randomWordToGuess[i] == input){
+              playerGuess[i] = input;
+            }
+          }
+          if(isTheWordGuessed(playerGuess)){
+            wordIsGuessed = true;
+            System.out.println("Congratulations! You won!");
+            }
+          }
+        }
+            if(!wordIsGuessed) System.out.println("You ran out of guesses:/");
+            System.out.println("Do you want to play another game? (yes/no)");
+            String anotherGame = scanner.nextLine();
+            if (anotherGame.equals("no")) weArePlaying = false;
+            }
+
+    System.out.println("Game over!");
+  }
+
+  public static void printArray(char[] array){
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static boolean isTheWordGuessed(char[] array){
+    for (int i = 0; i < array.length; i++) {
+      if(array[i] == '_') return false;
+    }
+      return true;
+  }
 
   public Hangman(String answer){
     //equate private variable tvalues to those in input
